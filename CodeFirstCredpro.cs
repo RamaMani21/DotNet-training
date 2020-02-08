85255b6f53a0114f71bbfa9f192a6b81359bea8d
RamaMani21 <60807350+RamaMani21@users.noreply.github.com>
2020-02-08 14:55:43 +0530

    Add files via upload
    C# EntityFramework

---
+++ CodeFirstCredpro.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+
+
+namespace CodeFirstCredpro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerDbContext context = new CustomerDbContext();
+            var orders = context.Orders;
+
+            var order1 = orders.Add(new Order()
+            {
+                OrderID = 1,
+                OrderDate = new DateTime(2020, 01, 01),
+                CustomerID = 1,
+                ShippedDate = new DateTime(2020, 01, 15)
+
+            });
+            var order2 = orders.Add(new Order()
+            {
+                OrderID = 2,
+                OrderDate = new DateTime(2020, 01, 10),
+                CustomerID = 2,
+                ShippedDate = new DateTime(2020, 01, 15)
+
+            });
+            context.SaveChanges();
+        }
+    }
+    public class CustomerDbContext : DbContext
+    {
+        public CustomerDbContext(): base("name=CustomerDbContext")
+        { }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string country { get; set; }
+    }
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int CustomerID { get; set; }
+        public DateTime ShippedDate { get; set; }
+    }
+}
---
+++ Orders.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderDBContext context = new OrderDBContext();
+            var orders = context.Orders;
+
+            var Order1 = order.Add(new Order() { OrderID = 1, ShipName = "zoe", ShipCity = "bern", CustomerID = 1 });
+            var Order2 = order.Add(new Order() { OrderID = 2, ShipName = "veer", ShipCity = "italy", CustomerID = 1 });
+            var Order3 = order.Add(new Order() { OrderID = 3, ShipName = "pat", ShipCity = "zurich", CustomerID = 1 });
+
+
+            context.SaveChanges();
+
+
+        }
+        public class OrderDBContext : DbContext
+        {
+            public OrderDBContext() :
+                 base("name=OrderDBContext")
+            { }
+
+            public DbSet<Order> Orders { get; set; }
+
+        }
+
+        public class Order
+        {
+            public int OrderID { get; set; }
+            public string ShipName { get; set; }
+            public string ShipCity { get; set; }
+            public int CustomerID { get; set; }
+        }
+
+    }
+}
+
