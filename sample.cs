12732752973d7a3fe5775176b37caf6df5f6f718
RamaMani21 <60807350+RamaMani21@users.noreply.github.com>
2020-02-08 12:00:55 +0530

    Add files via upload
    HTML_CSS_JavaScript

---
+++ sample.cs
@@ -0,0 +1,29 @@
+using system;
+
+namespace CSharpFundamentals
+{
+      public class sample  
+
+       {
+          public static void Main(string[] args)
+
+            {
+
+               int empAge;
+               string empName;
+
+                Console.WriteLine("Enter employee name\n");
+                empName = Console.ReadLine();
+                Console.WriteLine("Enter employee age\n");
+                empAge = int.Parse(Console.ReadLine());  
+                Console.WriteLine("Congratulations Mr/Miss {0} on your {1} Birthday", empName, empAge);
+
+                
+
+
+
+             }
+        }
+
+
+}
